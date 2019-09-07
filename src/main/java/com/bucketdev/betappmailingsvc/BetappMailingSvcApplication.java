23e84b90fadeab76e6a591597bad90c68591d88c
vanfigo <rodrigo.loyola@live.com>
2019-09-06 19:03:58 -0500

    first commit

---
+++ src/main/java/com/bucketdev/betappmailingsvc/BetappMailingSvcApplication.java
@@ -0,0 +1,13 @@
+package com.bucketdev.betappmailingsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetappMailingSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetappMailingSvcApplication.class, args);
+	}
+
+}
